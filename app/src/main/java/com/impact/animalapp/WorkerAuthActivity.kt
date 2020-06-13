6bffc02520d1e34b0ef24af52822f6af8bacc035
Ilya Ponomaryov <ip287733@gmail.com>
2020-06-13 15:43:10 +0500

    12

---
+++ app/src/main/java/com/impact/animalapp/WorkerAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.impact.animalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkerAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker_auth)
+    }
+}
